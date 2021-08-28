256c016b324ce3856412fb83845ba3119eb8d252
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2021-08-28 16:17:11 +0530

    Hero Section, Navbar and sidebar added

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+import { Button } from "../ButtonElement";
+import Video from "../../videos/video.mp4";
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay muted loop src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in credits towards
+          your next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to="sign-up" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
