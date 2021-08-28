638c63c1052cc63db836889fbf3655d751179e5e
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2021-08-28 16:07:55 +0530

    Hero, Navbar and Sidebar completed

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
+
+import { FaBars } from "react-icons/fa";
+
+const Navbar = ({ toggle }) => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">dolla</NavLogo>
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="sign-up">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+
+          <NavBtn>
+            <NavBtnLink to="sign-in">Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
